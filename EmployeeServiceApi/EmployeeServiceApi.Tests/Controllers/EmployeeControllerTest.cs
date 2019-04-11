ff4efcd4940bd0b022b61b539522190b210b743a
Babu Ayyappan <babu_pers@yahoo.com>
2019-04-11 11:55:34 +0530

    EmployeeService Project added

---
+++ EmployeeServiceApi/EmployeeServiceApi.Tests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeServiceApi;
+using EmployeeServiceApi.Controllers;
+
+namespace EmployeeServiceApi.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeeControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            IEnumerable<string> result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual("value1", result.ElementAt(0));
+            Assert.AreEqual("value2", result.ElementAt(1));
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            string result = controller.Get(5);
+
+            // Assert
+            Assert.AreEqual("value", result);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            controller.Put(5, "value");
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            EmployeeController controller = new EmployeeController();
+
+            // Act
+            controller.Delete(5);
+
+            // Assert
+        }
+    }
+}
