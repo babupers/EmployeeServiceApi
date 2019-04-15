fd528e7f9c15fbd35a316eea3ccf82b4ee87807f
Babu Ayyappan <babu_pers@yahoo.com>
2019-04-15 12:18:39 +0530

    Get Method & its test case changed

--- EmployeeServiceApi/EmployeeServiceApi/Controllers/EmployeeController.cs
+++ EmployeeServiceApi/EmployeeServiceApi/Controllers/EmployeeController.cs
@@ -12,7 +12,7 @@ namespace EmployeeServiceApi.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2", "value3","value4" };
         }
 
         // GET api/values/5
